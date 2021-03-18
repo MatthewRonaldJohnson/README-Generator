90c0915df1e7129b9ef05eb8ce6667d02894356a
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-03-17 19:53:11 -0500

    added basic functionality, need to add license tag

---
+++ Output/README.md
@@ -0,0 +1,31 @@
+
+        # Testing
+
+        ## Description
+
+        We're testing stuff
+        
+        ## Installation Instructions
+
+        Install it
+
+        ## Usage Information
+
+        use it
+
+        ## Contribution Guidelines
+
+        dont 
+
+        ## Testing Instructions
+
+        dont test
+
+        ## License
+
+        CopyLeft
+
+        ## Questions
+
+        Contact me via my github profile, MRJ, or email, MRJ
+     
--- index.js
+++ index.js
@@ -1,39 +1,106 @@
 // TODO: Include packages needed for this application
-
-const inquirer = require("inquirer");
-
+const inquirer = require('inquirer');
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = ['Enter the title', 'Enter a short description', 'Enter installtion instructions', 'Enter usage information', 'Enter contribution guidelines', 'Enter test instructions', 'Enter your Github username', 'Enter your email'];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter your projects title',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a short description for your project',
+        name: 'desc'
+    },
+    {
+        type: 'input',
+        message: 'Enter any installation instructions',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'Enter the usage information',
+        name: 'usageInfo'
+    },
+    {
+        type: 'input',
+        message: 'Enter the contribution guidelines',
+        name: 'contribute'
+    },
+    {
+        type: 'input',
+        message: 'Enter the testing instructions',
+        name: 'testing'
+    },
+    {
+        type: 'list',
+        message: 'Select a license for this project',
+        name: 'license',
+        choices: ['Public Domain', 'Permissive', 'LGPL', 'CopyLeft', 'Proprietary']
+    },
+    {
+        type: 'input',
+        message: 'Enter your Github Username',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address',
+        name: 'email'
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    //get responses stored in data array and print to fileName
+function writeToFile(content) {
+    fs.writeFile('./Output/README.md', content, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
 }
 
-function getData(){
-    //ask questions, loop through questions array
-    //store responses in answers array (indexes of answers and questions array match up)
-    const answers = []; //init answers as an array so we can use the push method
-    questions.forEach(question => {
-        inquirer
-            .prompt([question])
-            .then(response => answers.push(response))
-            .catch(error => { //this section comes from the inquirer docs
-                if (error.isTtyError) {
-                    console.log("Prompt couldn't be rendered in the current environment")
-                } else {
-                    console.log('something else went wrong')
-                }
-            })
-    })
-    return answers;
+function generateContent({title, desc, install, usageInfo, contribute, testing, license, github, email}){
+     return content = `
+        # ${title}
+
+        ## Description
+
+        ${desc}
+        
+        ## Installation Instructions
+
+        ${install}
+
+        ## Usage Information
+
+        ${usageInfo}
+
+        ## Contribution Guidelines
+
+        ${contribute}
+
+        ## Testing Instructions
+
+        ${testing}
+
+        ## License
+
+        ${license}
+
+        ## Questions
+
+        Contact me via my github profile, ${github}, or email, ${email}
+     `
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    //create readme file
-    const userInputs = getData();
-    writeToFile(readme, userInputs); //should we just do writeToFile(readme, getData()) ??
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            const content = generateContent(data);
+            writeToFile(content);
+        })
 }
 
 // Function call to initialize app
